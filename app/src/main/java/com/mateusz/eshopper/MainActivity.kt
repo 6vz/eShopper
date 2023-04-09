3d5d0cbaf93cfb20e792e4ff7a4efdc3ea314e3c
Matt <mateuszbaranowski@protonmail.com>
2023-04-09 20:36:39 +0200

    o... dziala... dzieki babcia z bigos, dodal mocy

--- app/src/main/java/com/mateusz/eshopper/MainActivity.kt
+++ app/src/main/java/com/mateusz/eshopper/MainActivity.kt
@@ -3,51 +3,81 @@ package com.mateusz.eshopper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private val MAX_TASKS = 10 // Maximum number of tasks that can be added
+    private var taskCount = 0 // Counter to keep track of number of tasks added
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // get what is input and button
-        var input = findViewById<EditText>(R.id.input)
-        var button = findViewById<Button>(R.id.button)
-        var tabPicker = findViewById<TabLayout>(R.id.picker)
 
-        var lista = arrayOf<String>()
+        // Get references to UI elements
+        val input = findViewById<EditText>(R.id.input)
+        val button = findViewById<Button>(R.id.button)
+        val tabPicker = findViewById<TabLayout>(R.id.picker)
 
-        button.setOnClickListener() {
-            var input = input.text.toString()
-            if (input.isEmpty()) {
+        // Get references to checkboxes
+        val c1 = findViewById<CheckBox>(R.id.c1)
+        val c2 = findViewById<CheckBox>(R.id.c2)
+        val c3 = findViewById<CheckBox>(R.id.c3)
+        val c4 = findViewById<CheckBox>(R.id.c4)
+        val c5 = findViewById<CheckBox>(R.id.c5)
+        val c6 = findViewById<CheckBox>(R.id.c6)
+        val c7 = findViewById<CheckBox>(R.id.c7)
+        val c8 = findViewById<CheckBox>(R.id.c8)
+        val c9 = findViewById<CheckBox>(R.id.c9)
+        val c10 = findViewById<CheckBox>(R.id.c10)
 
-                val builder = android.app.AlertDialog.Builder(this)
-                builder.setTitle("Error")
-                builder.setMessage("Please enter a valid input")
-                builder.setPositiveButton("OK", null)
-                val dialog: android.app.AlertDialog = builder.create()
-                dialog.show()
+        // Array to store tasks
+        val tasks = mutableListOf<String>()
+
+        // Set click listener on button to add task
+        button.setOnClickListener {
+            // Get input text
+            val task = input.text.toString().trim()
+
+            // Check if input is empty or if maximum number of tasks has been reached
+            if (task.isEmpty()) {
+                showDialog("Error", "Please enter a valid task")
+            } else if (taskCount >= MAX_TASKS) {
+                showDialog("Error", "You have reached the maximum limit of tasks")
             } else {
-                // get currently what's selected on Tab
-                var selected = tabPicker.selectedTabPosition
-                var category = ""
-                if (selected == 0) {
-                    category = "nabiał"
-                } else if (selected == 1) {
-                    category = "roślinne"
-                } else {
-                    category = "inne"
-                }
-                var product = "[${category}] ${input}"
-                lista += product
-                for (i in lista) {
-                    println(i)
-                    // add to recyclerView with id recyclerView
-                    
+                // Add task to tasks array and update task count
+                tasks.add(task)
+                taskCount++
+
+                // Update checkboxes with tasks
+                when (taskCount) {
+                    1 -> c1.text = task
+                    2 -> c2.text = task
+                    3 -> c3.text = task
+                    4 -> c4.text = task
+                    5 -> c5.text = task
+                    6 -> c6.text = task
+                    7 -> c7.text = task
+                    8 -> c8.text = task
+                    9 -> c9.text = task
+                    10 -> c10.text = task
                 }
 
+                // Clear input text
+                input.setText("")
             }
         }
+    }
 
+    // Helper function to show dialog
+    private fun showDialog(title: String, message: String) {
+        val builder = android.app.AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("OK", null)
+        val dialog: android.app.AlertDialog = builder.create()
+        dialog.show()
     }
 }
